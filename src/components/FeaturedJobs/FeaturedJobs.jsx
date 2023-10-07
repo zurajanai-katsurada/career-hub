15c3d59d0b1077fd4b428dd54b0fdf90609c81c3
zurajanai-katsurada <antusharma6624@gmail.com>
2023-10-07 15:41:21 +0600

    your new destination for all tech-related jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
+
 const FeaturedJobs = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
+
   return (
     <div>
       <div className="text-center">
-        <h2 className="text-5xl">Featured Jobs</h2>
+        <h2 className="text-5xl">Featured Jobs : {jobs}</h2>
         <p>
           Explore thousands of job opportunities with all the information you
           need. Its your future
