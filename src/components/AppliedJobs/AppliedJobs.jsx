6c162d040a066e1832b81439a25793ab8eddc80e
zurajanai-katsurada <antusharma6624@gmail.com>
2023-10-10 16:30:54 +0600

    your new destination for all tech-related jobs

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -7,6 +7,26 @@ const AppliedJobs = () => {
 
   const [appliedJobs, setAppliedJobs] = useState([]);
 
+  const [displayJobs, setDisplayJobs] = useState([]);
+
+  const handleJobsFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(appliedJobs);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Remote"
+      );
+
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Onsite"
+      );
+
+      setDisplayJobs(onsiteJobs);
+    }
+  };
+
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
 
@@ -24,14 +44,42 @@ const AppliedJobs = () => {
       }
 
       setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied);
 
       // console.log(jobs, storedJobIds, jobsApplied);
     }
-  }, []);
+  }, [jobs]);
 
   return (
     <div>
       <h2 className="text-2xl">Jobs I applied for: {appliedJobs.length}</h2>
+
+      <div className="text-left">
+        <details className="dropdown mb-32">
+          <summary className="m-1 btn">filter by</summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li onClick={() => handleJobsFilter("all")}>
+              <a>All</a>
+            </li>
+            <li onClick={() => handleJobsFilter("remote")}>
+              <a>Remote</a>
+            </li>
+            <li onClick={() => handleJobsFilter("onsite")}>
+              <a>Onsite</a>
+            </li>
+          </ul>
+        </details>
+      </div>
+
+      <ul>
+        {displayJobs.map((job) => (
+          <li key={job.id}>
+            <span>
+              {job.job_title} {job.company_name}: {job.remote_or_onsite}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
